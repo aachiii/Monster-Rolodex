546d0fa0f50ab3816d71326b2cf1e9e7f1c731ee
Chiqu Li <chiquli@163.com>
2020-06-28 17:01:47 -0400

    Updates

---
+++ precache-manifest.70a17b583bbe1e6cd405b58829e03c0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "270c6b8b04e69ffebc4d596871951c34",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "2619796f09c87605ac79",
+    "url": "/Monster-Rolodex/static/css/main.55dfdc16.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/Monster-Rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2619796f09c87605ac79",
+    "url": "/Monster-Rolodex/static/js/main.d93f3f77.chunk.js"
+  },
+  {
+    "revision": "3e2b7460a7751f8973f3",
+    "url": "/Monster-Rolodex/static/js/runtime-main.1aff1f47.js"
+  }
+]);
